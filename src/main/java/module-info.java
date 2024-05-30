be208f08ac8b27fd7cd271463ff07b2611a39d7e
typsoon <polarny.p@onet.pl>
2024-05-30 19:43:10 +0200

    Created project structure

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.idproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.idproject to javafx.fxml;
+    exports org.example.idproject;
+}
