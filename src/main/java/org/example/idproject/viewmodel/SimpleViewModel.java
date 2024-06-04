27ce2c79018bc99b8ace3bcb83701e07a5556909
RIPer <adam.burszka@student.uj.edu.pl>
2024-06-04 21:08:11 +0200

    more requests

--- src/main/java/org/example/idproject/viewmodel/SimpleViewModel.java
+++ src/main/java/org/example/idproject/viewmodel/SimpleViewModel.java
@@ -22,7 +22,7 @@ public class SimpleViewModel implements ViewModel{
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery( "select * from SearchPlayers(\'" + nickName + "\');");
             while (rs.next()) {
-                players.add(new BasicPlayerData(Integer.parseInt(rs.getString(1)),rs.getString(2)));
+                players.add(new BasicPlayerData(rs.getInt(1),rs.getString(2)));
                // System.out.println(rs.getString(1) + " " + rs.getString(2) );
             }
         }
@@ -32,14 +32,36 @@ public class SimpleViewModel implements ViewModel{
         return players;
     }
 
+
+
     @Override
-    public Collection<BasicPlayerData> getPlayers() {
+    public FullPlayerData getFullPlayerData(int playerId) {
+        try (Connection conn = DriverManager.getConnection(url, credentials.username(), credentials.password())) {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery( "select * from FullPlayerData where player_ID = (\'" + playerId + "\');");
+            if (rs.next()) {
+                return
+                new FullPlayerData(
+                        rs.getLong(1),rs.getString(2),rs.getString(3),
+                        new BasicPlayerData(rs.getInt(4),rs.getString(5))
+                );
+                // System.out.println(rs.getString(1) + " " + rs.getString(2) );
+            }
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    @Override
+    public Collection<BasicPlayerData> getAllPlayers() {
         Collection<BasicPlayerData> players = new ArrayList<>();
         try (Connection conn = DriverManager.getConnection(url, credentials.username(), credentials.password())) {
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery("select player_ID, PlayerNickname(player_ID) from players");
             while (rs.next()) {
-                players.add(new BasicPlayerData(Integer.parseInt(rs.getString(1)),rs.getString(2)));
+                players.add(new BasicPlayerData(rs.getInt(1),rs.getString(2)));
                 //System.out.println(rs.getString(1) + " " + rs.getString(2) );
             }
         }
@@ -50,19 +72,48 @@ public class SimpleViewModel implements ViewModel{
     }
 
     @Override
-    public FullPlayerData getFullPlayerData(int playerId) {
-        return null;
+    public boolean insertPlayer(String login, String password, String nickName) {
+        return false;
     }
 
     @Override
     public Collection<BasicClanData> browseClans(String name) {
-        return List.of();
+        Collection<BasicClanData> clans = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(url, credentials.username(), credentials.password())) {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from searchclans(\'" + name + "\');");
+            while (rs.next()) {
+                clans.add(new BasicClanData(rs.getInt(1),rs.getString(2)));
+                //System.out.println(rs.getString(1) + " " + rs.getString(2) );
+            }
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return clans;
     }
 
     @Override
     public FullClanData getFullClanData(int clanId) {
 
         return null;
+    }
+
+    @Override
+    public Collection<BasicClanData> getAllClans() {
+        Collection<BasicClanData> clans = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(url, credentials.username(), credentials.password())) {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select clan_id, clanname(clan_id) from clans");
+            while (rs.next()) {
+                clans.add(new BasicClanData(rs.getInt(1),rs.getString(2)));
+                //System.out.println(rs.getString(1) + " " + rs.getString(2) );
+            }
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return clans;
     }
 
     @Override
@@ -76,12 +127,12 @@ public class SimpleViewModel implements ViewModel{
            return false;
         }
     }
-//
-//    public static void main(String[] args) {
-//        SimpleViewModel simpleViewModel = new SimpleViewModel();
-//        simpleViewModel.tryLogIn(new Credentials("riper", "aaa"));
-//        Collection<BasicClanData> basicClanData = simpleViewModel.browseClans("clan1");
-//
-//        return;
-//    }
+
+    public static void main(String[] args) {
+        SimpleViewModel simpleViewModel = new SimpleViewModel();
+        simpleViewModel.tryLogIn(new Credentials("riper", "aaa"));
+        Collection<BasicClanData> clanData = simpleViewModel.browseClans("clan1");
+
+        return;
+    }
 }
