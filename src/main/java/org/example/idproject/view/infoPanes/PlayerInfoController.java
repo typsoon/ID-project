c68a5d94f5494f41670052e45b627d901e0e7e80
typsoon <polarny.p@onet.pl>
2024-06-07 20:48:59 +0200

    Added init.shl

--- src/main/java/org/example/idproject/view/infoPanes/PlayerInfoController.java
+++ src/main/java/org/example/idproject/view/infoPanes/PlayerInfoController.java
@@ -1,5 +1,8 @@
 package org.example.idproject.view.infoPanes;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import org.example.idproject.common.FullPlayerData;
 import org.example.idproject.view.ScreenManager;
 import org.example.idproject.viewmodel.DatabaseService;
 
@@ -8,6 +11,10 @@ public class PlayerInfoController extends AbstractInfoController {
         super(databaseService, screenManager);
     }
 
+    @FXML private Text currentClanName;
+
+    @FXML private Text currentNickname;
+
     @Override
     protected void initialize() {
 
@@ -15,6 +22,9 @@ public class PlayerInfoController extends AbstractInfoController {
 
     @Override
     public void update(int id) {
+        FullPlayerData fullPlayerData = databaseService.getFullPlayerData(id);
 
+        currentClanName.setText(fullPlayerData.currentClanName() == null ? "" : fullPlayerData.currentClanName());
+        currentNickname.setText(fullPlayerData.basicPlayerData().currentNickname());
     }
 }
